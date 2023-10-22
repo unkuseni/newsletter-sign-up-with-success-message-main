c18697eee2a6cf5eff1f25feb0f71a276566e026
unkuseni <amudalatadeoye@gmail.com>
2023-10-22 23:09:26 +0100

    done with this

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-const card = document.getElementById('card');
-const formSection = document.getElementById('form-section');
-const form = document.getElementById('form');
-const successSection = document.getElementById('success');
-const emailInput = document.getElementById('email');
-let emailValue = emailInput.value;
-const emailValidation = () => {
-  if (emailValue.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
-    console.log('Email is valid');
-    formSection.style.display = 'none';
-  } else {
 
-    console.log('Email is not valid');
-  }
-}
+const card = document.getElementById("card");
+const formSection = document.getElementById("form-section");
+const form = document.getElementById("form");
+const successSection = document.getElementById("success");
+const emailInput = document.getElementById("email");
+const dismiss = document.getElementById("dismiss-btn");
+const errorMsg = document.getElementById('error');
+let emailValue = emailInput.value;
+successSection.style.display = "none";
+errorMsg.style.display = 'none';
+
+
+
+const emailValidation = () => {
+	const emailValue = emailInput.value.trim();
+	const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
+
+	if (isValidEmail) {
+		console.log("Email is valid");
+		formSection.style.display = "none";
+		successSection.style.display = "flex";
+		card.classList.add('success');
+	} else {
+		console.log("Email is invalid");
+		emailInput.classList.add('error');
+		errorMsg.style.display = 'block';
+	}
+};
+
+const dismissFn = () => {
+	formSection.style.display = "flex";
+	successSection.style.display = "none";
+	errorMsg.style.display = 'none';
+	emailInput.classList.remove("error");
+	card.classList.remove('success');
+};
+
 // Check for form submission
-form.addEventListener('submit', (event) => {
-  emailValidation();
-  event.preventDefault();
-  // Perform form submission logic here
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	emailValidation();
+	// Perform form submission logic here
+});
+
+// listen for dismiss-btn
+dismiss.addEventListener("click", () => {
+	dismissFn();
 });
